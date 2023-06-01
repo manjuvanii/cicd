c291cb61c22e67ad69117e8ac9ce2a14b489a0dd
peyyala7hills <101161227+peyyala7hills@users.noreply.github.com>
2023-06-01 10:38:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Hope you are doing well success !!');
+  res.send('Hello World, Hope you are doing well success for sonata !!');
 });
 
 app.listen(PORT, HOST);
